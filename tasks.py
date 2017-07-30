a91b2f942b4f669978cd77934cc8190621c0d959
paulmcevoy <paulmcevoy@gmail.com>
2017-07-30 19:00:22 +0100

    Script tidy up

--- tasks.py
+++ tasks.py
@@ -2,7 +2,6 @@ from celery.task import task
 import os
 @task
 def post_process():
-    os.system("./hello_paul.py")
     os.system("./sentiment_process_nltk.py")
     os.system("./sentiment_process_aylien.py")
     os.system("./similarity_nltk.py")
