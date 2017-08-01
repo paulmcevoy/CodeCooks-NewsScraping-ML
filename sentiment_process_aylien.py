4f09e073a5b27d16d15d6339a16fd3d852d71a1a
paulmcevoy <paulmcevoy@gmail.com>
2017-08-01 14:28:01 +0100

    Converted datetime.date to datetime to make it more granular

--- sentiment_process_aylien.py
+++ sentiment_process_aylien.py
@@ -9,8 +9,7 @@ import time
 from get_table_data import get_table_data
 from nltk import tokenize
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-from datetime import date
-
+import datetime
 def get_aylien(df_sample):
     conn, cursor = get_conn_info()
     c = textapi.Client("f40063af", "ddd790cf8730e5934f0a416f4ac44b2a")
@@ -63,20 +62,19 @@ def get_aylien(df_sample):
 
 df_art_table, df_ent_table, df_ent_table_norm, df_url_table = get_table_data()
 
-adate = date(2017, 7, 28)
+adate = datetime.date(2017, 7, 28)
+print ('Today  :', datetime.datetime.today())
 df_table_date = df_art_table[df_art_table.addedonpy > adate]
 
-
+"""
 df_table_date_aylien = df_table_date[df_art_table.aylien_sentiment_adv == False]
-#df_table_date_aylien = df_table_date_aylien[5:7]
 df_table_date_aylien_set = set(df_table_date_aylien['addedon'])
 
 print("Articles to analyse: {}".format(len(df_table_date_aylien)))
 for eachdate in df_table_date_aylien_set:
     df_table_date_aylien_one_day = df_table_date_aylien[(df_table_date_aylien.addedon == eachdate) ] 
     get_aylien(df_table_date_aylien_one_day)
-
-#print("{} articles processed by Aylien".format(loop_count))
+"""
 
 
 
